fd09effeeff3be951245b32292e9c2c0fb4f42c4
ecamtest <onizeoze@yahoo.fr>
2015-08-30 00:47:08 +0200

    Modif DB

--- minichat.sql
+++ minichat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 29 Août 2015 à 01:07
+-- Généré le :  Dim 30 Août 2015 à 00:46
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -17,21 +17,22 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `test`
+-- Base de données :  `minichat`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `minichat`
+-- Structure de la table `messages`
 --
 
-CREATE TABLE IF NOT EXISTS `minichat` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(255) NOT NULL,
   `message` text NOT NULL,
+  `date_msg` datetime NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
