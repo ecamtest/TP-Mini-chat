ad590b37e7ebe6017b358a1cd2d102f5eeeb895c
ecamtest <onizeoze@yahoo.fr>
2015-08-29 02:17:42 +0200

    Bêta

--- minichat.php
+++ minichat.php
@@ -1,1 +1,27 @@
+<html>
+
+ <head>
+
+ <title>Mini-chat</title>
+
+ </head>
+
+
+
+ <body>
+
+	<form style="text-align:center; width:50px;margin-left:40%" action="minichat_post.php" method="post">
+		<input style="width:150px;" name="pseudo" type="text" placeholder="Pseudo"/>
+		<input style="margin-top:5px;width:150px;" name="message" type="text" placeholder="Message"/>
+		<button style="margin-top:10px;width:70px;margin-left:40px" name="envoyer" type="submit">Envoyer</button>
+	</form>
+	
+	<div style="width:50%; border:solid 1px; height:75%">
+	</div>
  
+ </body>
+
+ </html>
+
+
+
--- minichat_post.php
+++ minichat_post.php
@@ -1,1 +1,31 @@
- 
+<?php
+	try
+	{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	}
+	catch (Exception $e)
+	{
+        die('Erreur : ' . $e->getMessage());
+	}
+	
+	$pseudo = htmlspecialchars($_POST['pseudo']);
+	$message = htmlspecialchars($_POST['message']);
+	$messages = '';
+	
+	$req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message');
+	
+	$req->execute(array(
+    'pseudo' => $pseudo,
+    'message' => $message,
+    ));
+	
+	$reponse = $bdd->query("select * from minichat"); //order by ID desc limit l0
+	$donnees = $reponse->fetch();
+	
+	while ($donnees = $reponse->fetch())
+	{
+		$messages+="<p>".$donnees['pseudo']." : ".$donnees['message']."</p>";
+	}
+	
+	header('Location: minichat.php');
+?>
